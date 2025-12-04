9b5a3387a8c7734ac581e22038ecb3b94b78605f
MusaMacheke <musamacheke.01@gmail.com>
2025-12-04 17:15:20 +0200

    Comment out LinkedIn and WhatsApp links
    Comment out LinkedIn and WhatsApp links in the footer.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
       <h4>Contact Me</h4>
       <div className='footerLinks'>
         <a href="https://github.com/MusaMacheke" target='_blank' rel="noopener noreferrer" aria-label="GitHub"><FaGithub /></a>
-        <a href="https://www.linkedin.com/in/musa-macheke-2abba2315/" target='_blank' rel="noopener noreferrer" aria-label="LinkedIn"><FaLinkedin /></a>
+        {/* <a href="https://www.linkedin.com/in/musa-macheke-2abba2315/" target='_blank' rel="noopener noreferrer" aria-label="LinkedIn"><FaLinkedin /></a> */}
         <a href='mailto:musamacheke.01@gmail.com' aria-label="Email"><GrMail /></a>
         <a href='tel:0640668708' aria-label="Phone"><FaPhone /></a>
         {/* <a href='https://wa.me/0640668708' target='_blank' rel="noopener noreferrer" aria-label="WhatsApp"><FaWhatsapp /></a> */}
